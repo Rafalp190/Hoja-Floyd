2a9f23b080fd605926bca418349b1b24629b4573
JulioBarahona <julio.fbm96@gmail.com>
2016-11-14 23:14:37 -0600

    Bathubs - RoyalCanoe
    >??????<

---
+++ NodeClass.java
@@ -0,0 +1,66 @@
+/**
+* @file NodeClass.java
+*/
+
+public class NodeClass{
+
+	// Comienzo Atributos 
+    public static final int errorEnIndice = -1;   
+    private int indice;
+    private String nombreReferencia;
+	
+	// Fin de Atributos 
+	
+	// Comienzo metodos
+    /**
+     * @param nombreReferencia This NodeClass's nombreReferencia.
+     */
+    public NodeClass(String nombreReferencia)
+    {
+        indice = errorEnIndice;
+        this.nombreReferencia = nombreReferencia;
+    }
+    /**
+     * @param indice
+     * @param nombreReferencia
+     */
+    public NodeClass(String nombreReferencia, int indice)
+    {
+        this.indice = indice;
+        this.nombreReferencia = nombreReferencia;
+    }
+	
+	
+    // return indice 
+    public int getindice()
+    {
+        return indice;
+    }
+	
+    /**
+     * @param indice New value for this NodeClass's indice.
+     */
+    public void setindice(int indice){
+        this.indice = indice;
+    }
+	
+	
+    // gets nombre 
+    public String getnombreReferencia(){
+        return nombreReferencia;
+    }
+	
+    /**
+     * @param nombreReferencia New value for this NodeClass's nombreReferencia.
+     */
+    public void setnombreReferencia(String nombreReferencia){
+        this.nombreReferencia = nombreReferencia;
+    }
+	
+	// To string 
+    public String toString(){
+        return nombreReferencia + " el indice es " + indice + " . "; 
+    }
+	
+	// Find de metodos
+}
